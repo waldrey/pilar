780d09c46785bdcfe118f51105e1159c85c4d596
Waldrey S. Silva <waldrey22@gmail.com>
2024-06-14 11:37:31 -0300

    feat: adding ruff linter (#1)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import os
+
 from dotenv import load_dotenv
 from fastapi import FastAPI, responses
 
 load_dotenv()
 
-app = FastAPI(title=os.getenv("APP_NAME", "Pilar API"))
+app = FastAPI(title=os.getenv('APP_NAME', 'Pilar API'))
 
-@app.get("/", include_in_schema=False)
+
+@app.get('/', include_in_schema=False)
 def root():
-    return responses.RedirectResponse(url='/docs')
+	return responses.RedirectResponse(url='/docs')
